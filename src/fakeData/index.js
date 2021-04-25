8f9ce834c6e93ef89905d1006257ecf0a1e0e591
Ziaul-karim <karimziaul62@gmail.com>
2021-04-25 23:19:10 +0600

    Final

--- src/fakeData/index.js
+++ src/fakeData/index.js
@@ -3,7 +3,7 @@ const differentLocations = [
     "id": "1",
     "name": "Cox's Bazar",
     "image": "https://i.ibb.co/6Rx1PCZ/Sajek.png",
-    "description": "Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.",
+    "description": "For Bangladeshis it doesn't get much better than Cox's Bazar, the country's most popular beach resort. Sort of a Cancun of the east, it's choc-a-bloc with massive cement hotels and gaudy over-development catering largely to the country's elite. To a foreigner it likely won't compare to other beach vacations you may have taken, but it's still interesting to see how the Bangladeshi's live it up. The beach is crowded, especially near the Hotel Motel Zone, a large cluster of more upmarket hotels. Expect lots of attention, and expect to stay fully clothed. 30 km south is Inani Beach. Things should be quieter here, but still expect to draw some attention.You can also check out the Burmese Market, but don't expect a lot of handicrafts. The traditional Burmese dresses and fabrics are definitely interesting. Besides that, you will find some stores which sell pearl jewellery. You can also try some local beauty products (sandal wood based), hand woven textile and bedsheets among many other things.",
     "hotels": [
       {
         id: 0,
@@ -41,7 +41,7 @@ const differentLocations = [
     "id": "2",
     "name": "Sreemangal",
     "image": "https://i.ibb.co/Qf3Y32p/Sreemongol.png",
-    "description": "It is said the destination Sreemangal (or Srimangal) is destinationd after Sri Das and Mangal Das; two brothers who settled on the banks of the Hail Haor.[2] A copper plate of Raja Marundanath from the 11th century was found in Kalapur. During an excavation at Lamua, an ancient statue of Ananta Narayan was dug out. In 1454, the Nirmai Shiva Bari was built and still stands today.",
+    "description": "Srimangal/Sreemangal is a city in Sylhet Division in Bangladesh. Sreemangal is situated in Moulvibazar district in sylhet division. Sreemangal is an Upazila. It is famous for tea garden. Rain all time occurs here. Nature has adorned sreemangal with green tress. Its natural scenery is very charming. It soothes one’s eyes. Birds are twittering always here. The first tea garden in Bangladesh which names “Malni chho ra tea garden” is here. Only one tea research institute in Bangladesh which name is BTRI (Bangladesh Tea Research Institute) is situated in Sreemangal. The terraced tea gardens, plantations and evergreen hills of sreemangal are wonderful treats for the tourists. On the way, of Sreemangal by bus you’ll see a statue “cha konna” which is stands for welcome you. But if you come in sreemangal by train you’ll miss this statue. When you’ll come near in the tea garden you find the smell of tea leaves. When you’ll reach sreemangal you get many sorts of hotels. Tea Resort & Rest House also available here.",
     "hotels": [
       {
         id: 0,
@@ -77,10 +77,10 @@ const differentLocations = [
   },
   {
     "id": "3",
-    "name": "Sundarbans",
+    "name": "Sundarban",
     "image": "https://i.ibb.co/52bY9LB/sundorbon.png",
     "profile": "https://i.ibb.co/PYscqbL/bfa7be922fb74a04bf45a5db4b723418.jpg",
-    "description": "The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh.",
+    "description": "Sundarban is the biggest mangrove forest in the world. Sundarban is in South West part of Bangladesh, in the district of greater Khulna. India shares a bit of the forest with Bangladesh. The total area is about 38,000 square kilo meters. Sundarban is a large block of littoral forests. The beauty lies in its unique natural surrounding. The Sundarbans are a part of the world's largest delta formed by the rivers Ganges, Brahmaputra and Meghna. Thousands of meandering streams, creeks, rivers and estuaries have enhanced its charm. For miles and miles, the lofty treetops form an unbroken canopy, while nearer the ground, works of high and ebb-tide marked on the soil and tree trunks and the many varieties of the natural mangrove forest have much to offer to an inquisitive visitor.",
     "hotels": [
       {
         id: 0,
